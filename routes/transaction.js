4fe712eebc45c54e62eba798bff796e0fb9b8594
Qiuyang Wang <qiuyangwang@brandeis.edu>
2023-04-23 22:24:27 -0400

    updated

---
+++ routes/transaction.js
@@ -0,0 +1,123 @@
+/*
+  transaction.js -- Router for the transaction
+*/
+
+const express = require('express');
+const router = express.Router();
+const TransactionItem = require('../models/TransactionItem')
+const User = require('../models/User')
+
+/*
+this is a very simple server which maintains a key/value
+store using an object where the keys and values are lists of strings
+
+*/
+
+isLoggedIn = (req, res, next) => {
+    if (res.locals.loggedIn) {
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
+
+router.get('/transaction/',
+    isLoggedIn,
+    async (req, res, next) => {
+        const sortBy = req.query.sortBy
+        let items = []
+        if(sortBy=='amount'){
+            items =
+                await TransactionItem.find({ userId: req.user._id }).sort({ amount: -1 })
+        } else if(sortBy=='category'){
+            items =
+                await TransactionItem.find({ userId: req.user._id }).sort({ category: 1 })
+        } else if(sortBy=="description"){
+            items =
+                await TransactionItem.find({ userId: req.user._id }).sort({ description: -1 })
+        } else if(sortBy=="date"){
+            items =
+                await TransactionItem.find({ userId: req.user._id }).sort({ date: -1 })
+        } else {
+            items =
+                await TransactionItem.find({ userId: req.user._id })
+        }
+
+        res.render('transactionList', { items });
+    }
+)
+
+router.post('/transaction/',
+    isLoggedIn,
+    async (req, res, next) => {
+        date = new Date(req.body.date.split("/")[2], req.body.date.split("/")[0] - 1, req.body.date.split("/")[1])
+        options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+        showdate = date.toLocaleDateString('en-US', options)
+        const transaction = new TransactionItem(
+            {
+                description: req.body.description,
+                amount: parseInt(req.body.amount),
+                category: req.body.category,
+                date: showdate,
+                userId: req.user._id,
+                username: req.user.username
+            })
+        await transaction.save();
+        res.redirect('/transaction')
+    }
+)
+
+router.get('/transaction/remove/:itemId',
+    isLoggedIn,
+    async (req, res, next) => {
+        await TransactionItem.deleteOne({ _id: req.params.itemId })
+        res.redirect('/transaction')
+    }
+)
+
+router.get('/transaction/edit/:itemId',
+    isLoggedIn,
+    async (req, res, next) => {
+        const item =
+            await TransactionItem.findById(req.params.itemId);
+        res.locals.item = item
+        res.render('edit')
+    }
+)
+
+router.post('/transaction/updateTransactionItem',
+    isLoggedIn,
+    async (req, res, next) => {
+        const { itemId, description, amount, category, date } = req.body;
+        await TransactionItem.findOneAndUpdate(
+            { _id: itemId },
+            { $set: { description, amount, category, date } });
+        res.redirect('/transaction')
+    }
+)
+
+router.get('/transaction/groupByCategory',
+    isLoggedIn,
+    async (req, res, next) => {
+        let results =
+            await TransactionItem.aggregate(
+                [
+                    {
+                        $match: { username: req.user.username }
+                    },
+                    {
+                        $group: {
+                            _id: '$category',
+                            total: { $sum: '$amount' },
+                        }
+                    },
+                    {
+                        $sort: { total: -1 }
+                    }
+                ]
+            )
+        res.render('summarizeModel', { results })
+    }
+)
+
+module.exports = router;
